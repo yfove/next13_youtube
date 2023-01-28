bd948f9edbf5f653f08b928d3e9e06776563fa2c
Curtis <curtis.ch.chat@gmail.com>
2023-01-27 17:57:03 -0700

    nextjs link and image import

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,28 @@
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "./page.module.css";
+import Movie from "./Movie";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default async function Home() {
-  const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`);
+  const data = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
+  );
   const res = await data.json();
   console.log(res);
   return (
     <main className={styles.main}>
       <h1>Hello next 13 🔥 </h1>
+      {res.results.map((movie) => (
+        <Movie
+          key={movie.id}
+          id={movie.id}
+          title={movie.title}
+          poster_path={movie.poster_path}
+          release_date={movie.release_date}
+        />
+      ))}
     </main>
   );
 }
